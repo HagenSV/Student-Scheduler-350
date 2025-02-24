b8225198dd9fc3f969e8ad595d2e9c4b72d14ebf
WHALEYDS22 <whaleyds22@gcc.edu>
2025-02-23 20:01:21 -0500

    Updated Course

--- src/main/java/edu/gcc/Course.java
+++ src/main/java/edu/gcc/Course.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 public class Course {
     private String name;
+
     private String description;
     private int[] startTime;
     private boolean MWForTR;
@@ -12,8 +13,12 @@ public class Course {
     private ArrayList<String> professors;
     private boolean isOpen;
     private boolean[] daysMeet;
+    private String department;
+    private String courseCode;
+    private int credits;
+    private int numSeats;
     private URL rateMyProfessorLink;
-    public Course(String name, int[] startTime, int[] duration, boolean isOpen, ArrayList<String> professors, boolean MWForTR, boolean[] daysMeet) {
+    public Course(String name, int[] startTime, int[] duration, boolean isOpen, ArrayList<String> professors, boolean MWForTR, boolean[] daysMeet, String department, String courseCode, int credits, int numSeats) {
         this.name = name;
         this.startTime = startTime;
         this.duration = duration;
@@ -21,14 +26,22 @@ public class Course {
         this.professors = professors;
         this.isOpen = isOpen;
         this.daysMeet = daysMeet;
+        this.department = department;
+        this.courseCode = courseCode;
+        this.credits = credits;
+        this.numSeats = numSeats;
     }
 
     public URL getRateMyProfessorLink(){return null;}
     public void setRateMyProfessorLink(){}
 
+    public int getCredits(){return credits;}
     public String getName() {
         return name;
     }
+    public int getNumSeats(){return numSeats;}
+    public String getDepartment(){return department;}
+    public String getCourseCode(){return courseCode;}
     public boolean getIsOpen(){return isOpen;}
     public String getDescription() {
         return description;
