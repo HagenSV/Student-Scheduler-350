8ec564483ccc95e5b830c9c4f2d162ae3a4d7044
HAGENSV22 <hagensv22@gcc.edu>
2025-03-03 21:29:28 -0500

    Create separate methods for each command

--- src/main/java/edu/gcc/ConsoleDriver.java
+++ src/main/java/edu/gcc/ConsoleDriver.java
@@ -6,6 +6,9 @@ import java.util.Scanner;
 
 public class ConsoleDriver {
 
+    private static final Scanner s = new Scanner(System.in);
+    private static User currentUser = null;
+
     //TODO: getUsers()
     public static List<User> getUsers(){
         return new ArrayList<>();
@@ -23,9 +26,6 @@ public class ConsoleDriver {
 
     public static void run() {
         String cmd = "";
-        Scanner s = new Scanner(System.in);
-
-        User currentUser = null;
 
         //Loop until account is found
         while (currentUser == null) {
@@ -61,94 +61,25 @@ public class ConsoleDriver {
             String[] input = s.nextLine().split(" ");
             cmd = input[0];
             switch (cmd) {
-                case "exit":
-                    break;
                 case "help":
-                    //Print list of commands
-                    System.out.println("  add <id> - adds class to schedule");
-                    System.out.println("  remove <id> - removes class from schedule");
-                    System.out.println("  courses - display list of users classes");
-                    System.out.println("  calendar - display schedule as calendar");
-                    System.out.println("  search - search for classes");
-                    System.out.println("  exit - exits the program");
+                    help(input);
                     break;
                 case "list":
-                    //Prints a list of the classes the user is in
-                    Course[] enrolled = currentUser.getSchedule().getCourses();
-                    for (Course c : enrolled){
-                        //Needs to string defined
-                        System.out.println(c);
-                    }
+                    listCourses(input);
                     break;
                 case "calendar":
-                    //Prints a calendar representation of the schedule
-                    System.out.println("  Time   |   Mon   |   Tue   |   Wed   |   Thu   |   Fri   |");
-                    for (int i = 0; i < 55; i++){
-                        int currentTime = i*15;
-                        System.out.print(formatTime(currentTime));
-                        System.out.print(" |");
-                        for (int j = 0; j < 5; j++){
-                            boolean timeFilled = false;
-                            for (Course c : currentUser.getSchedule().getCourses()){
-                                int startTime = c.getStartTime()[j];
-                                if (startTime >= currentTime && startTime+c.getDuration() <= currentTime){
-                                    //TODO get and display course dept and code
-                                    System.out.print(c.getDepartment());
-                                    System.out.print(c.getCourseCode());
-                                    System.out.print(c.getSection());
-                                    System.out.print("|");
-                                    timeFilled = true;
-                                }
-                            }
-                            if (!timeFilled) {
-                                System.out.print("         |");
-                            }
-                        }
-                        System.out.println();
-                    }
+                    printCalendar(input);
                     break;
                 case "search":
-                    //TODO: implement this when search is working
-                    System.out.println("Not implemented");
+                    search(input);
                     break;
                 case "add":
-                    //Adds class to schedule
-                    if (input.length < 2){
-                        System.out.println("Proper usage: add <course_id>");
-                        break;
-                    }
-                    try {
-                        int cid = Integer.parseInt(input[1]);
-                        Course add = getCourse(cid);
-                        if (add != null) {
-                            currentUser.getSchedule().addCourse(add);
-                            currentUser.saveSchedule();
-                        }
-                    } catch (NumberFormatException e){
-                        System.out.printf("Error: %s is not a number\n",input[1]);
-                    } catch (IndexOutOfBoundsException e){
-                        System.out.println("Invalid course id "+input[1]);
-                    }
+                    addCourse(input);
                     break;
-
                 case "remove":
-                    //Removes class from schedule
-                    if (input.length < 2){
-                        System.out.println("Proper usage: remove <course_id>");
-                        break;
-                    }
-                    try {
-                        int cid = Integer.parseInt(input[1]);
-                        Course remove = getCourse(cid);
-                        if (remove != null) {
-                            currentUser.getSchedule().removeCourse(remove);
-                            currentUser.saveSchedule();
-                        }
-                    } catch (NumberFormatException e){
-                        System.out.printf("Error: %s is not a number\n",input[1]);
-                    } catch (IndexOutOfBoundsException e){
-                        System.out.println("Invalid course id "+input[1]);
-                    }
+                    removeCourse(input);
+                    break;
+                case "exit":
                     break;
                 default:
                     //Provide feedback on invalid command
@@ -158,6 +89,113 @@ public class ConsoleDriver {
         System.out.println("Thank you for using Student Scheduler");
     }
 
+    /**
+     * Prints a list of commands
+     * @param options
+     */
+    private static void help(String[] options){
+        System.out.println("  add <id> - adds class to schedule");
+        System.out.println("  remove <id> - removes class from schedule");
+        System.out.println("  courses - display list of users classes");
+        System.out.println("  calendar - display schedule as calendar");
+        System.out.println("  search - search for classes");
+        System.out.println("  exit - exits the program");
+    }
+
+    private static void search(String[] options){
+        //TODO: implement this when search is working
+        System.out.println("Not implemented");
+    }
+
+    /**
+     * Prints a list of the users classes
+     * @param options
+     */
+    private static void listCourses(String[] options){
+        Course[] enrolled = currentUser.getSchedule().getCourses();
+        for (Course c : enrolled){
+            //Needs to string defined
+            System.out.println(c);
+        }
+    }
+
+    /**
+     * Prints a calendar representation of the schedule
+     * @param options
+     */
+    private static void printCalendar(String[] options){
+        System.out.println("  Time   |   Mon   |   Tue   |   Wed   |   Thu   |   Fri   |");
+        for (int i = 0; i < 55; i++){
+            int currentTime = i*15;
+            System.out.print(formatTime(currentTime));
+            System.out.print(" |");
+            for (int j = 0; j < 5; j++){
+                boolean timeFilled = false;
+                for (Course c : currentUser.getSchedule().getCourses()){
+                    int startTime = c.getStartTime()[j];
+                    if (startTime >= currentTime && startTime+c.getDuration() <= currentTime){
+                        //TODO get and display course dept and code
+                        System.out.print(c.getDepartment());
+                        System.out.print(c.getCourseCode());
+                        System.out.print(c.getSection());
+                        System.out.print("|");
+                        timeFilled = true;
+                    }
+                }
+                if (!timeFilled) {
+                    System.out.print("         |");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * Adds a course to the user's schedule
+     * @param options
+     */
+    private static void addCourse(String[] options){
+        if (options.length < 2){
+            System.out.println("Proper usage: add <course_id>");
+            return;
+        }
+        try {
+            int cid = Integer.parseInt(options[1]);
+            Course add = getCourse(cid);
+            if (add == null){ return; }
+            currentUser.getSchedule().addCourse(add);
+            currentUser.saveSchedule();
+        } catch (NumberFormatException e){
+            System.out.printf("Error: %s is not a number\n",options[1]);
+        }
+    }
+
+    /**
+     * Removes a course from the user's schedule
+     * @param options
+     */
+    private static void removeCourse(String[] options){
+        if (options.length < 2){
+            System.out.println("Proper usage: remove <course_id>");
+            return;
+        }
+        try {
+            int cid = Integer.parseInt(options[1]);
+            Course remove = getCourse(cid);
+            if (remove == null) { return; }
+
+            currentUser.getSchedule().removeCourse(remove);
+            currentUser.saveSchedule();
+        } catch (NumberFormatException e){
+            System.out.printf("Error: %s is not a number\n",options[1]);
+        }
+    }
+
+    /**
+     *
+     * @param time offset (in minutes) from 8 AM
+     * @return formatted time string
+     */
     private static String formatTime(int time){
         int hour = time/60+8;
         int minute = time%60;
