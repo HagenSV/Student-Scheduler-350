9f18e2a6af567b99679d5717cfbf1430b4316f7b
isaacbusler <isaacbusler@gmail.com>
2025-03-13 15:36:30 -0400

    small search fix2 - isaac

--- src/main/java/edu/gcc/Main.java
+++ src/main/java/edu/gcc/Main.java
@@ -17,14 +17,10 @@ public class Main {
 
     public static void main(String[] args) {
         getCourses();
-        //for (Course c : courses) {
-        //    System.out.println(c.toString());
-        //}
-
-        Search s = new Search("soci");
-        s.search();
-        System.out.println(s.getResult());
-        //ConsoleDriver.run();
+        for (Course c : courses) {
+            System.out.println(c.toString());
+        }
+        ConsoleDriver.run();
     }
 
     /**
