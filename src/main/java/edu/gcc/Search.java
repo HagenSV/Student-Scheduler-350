dd772a83f2fff8cf3b796a7bcbbef5f206ac9221
isaacbusler <isaacbusler@gmail.com>
2025-03-21 15:15:17 -0400

    tolowercase fixed - isaac

--- src/main/java/edu/gcc/Search.java
+++ src/main/java/edu/gcc/Search.java
@@ -402,7 +402,7 @@ public class Search {
         String toUseQuery = query;
         for (Course c : initialResult) {
             String code = c.getDepartment() + " " + c.getCourseCode();
-            if (query.contains(code.toLowerCase()) || query.contains(c.getName().toLowerCase()) && !temp.contains(c)) {
+            if (query.toLowerCase().contains(code.toLowerCase()) || query.toLowerCase().contains(c.getName().toLowerCase()) && !temp.contains(c)) {
                 temp.add(c);
                 if (toUseQuery.contains(code.toLowerCase())) {
                     toUseQuery = toUseQuery.replace(code.toLowerCase(), "");
@@ -453,7 +453,6 @@ public class Search {
      * @param query the search query
      */
     public void searchByDepartment(String query)    {
-
         for (String s : listDep)    {
             for (String d : query.split(" ")) {
                 if (s.toLowerCase().equals(d.toLowerCase()))    {
