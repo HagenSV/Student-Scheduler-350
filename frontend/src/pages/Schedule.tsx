b81b9d8973f012edff94c8e64c62c4b7ef714825
HAGENSV22 <hagensv22@gcc.edu>
2025-04-02 13:43:00 -0400

    Basic schedule page

---
+++ frontend/src/pages/Schedule.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Schedule = () => {
+    return (
+        <div>
+        <h1>My Schedule</h1>
+        <p>Here is where you can view your schedule.</p>
+        <h1 id="export">Export</h1>
+        </div>
+    );
+}
+
+export default Schedule;
