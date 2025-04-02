102c93cc5b4c7ca095ef26d8c861ef7e112daad8
HAGENSV22 <hagensv22@gcc.edu>
2025-04-02 14:28:16 -0400

    Update schedule page template

--- frontend/src/pages/Schedule.tsx
+++ frontend/src/pages/Schedule.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 const Schedule = () => {
     return (
-        <div>
+        <main>
         <h1>My Schedule</h1>
         <p>Here is where you can view your schedule.</p>
+        <h1>Classes</h1>
+        <p>This is where you can see a list of your classes</p>
         <h1 id="export">Export</h1>
-        </div>
+        </main>
     );
 }
 
