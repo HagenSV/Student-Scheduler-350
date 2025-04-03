f022b6fb3dbff749f0e4fddbc906f1db98a7a370
HAGENSV22 <hagensv22@gcc.edu>
2025-04-03 09:29:42 -0400

    Build out search page

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
 
+import CoursePreview from '../components/course_preview/CoursePreview.tsx';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const example = {
+    title: "Introduction to Computer Science",
+    department: "COMP",
+    code: 155,
+    section: "B",
+    credits: 3,
+    description: "This course is an introduction to computer science.",
+    professor: "Dr. Dickinson"
+}
+
 const Search = () => {
     return (
         <main>
-        <h1>Course Search</h1>
+        <Container>
+            <Row>
+                <Col md={6}>
+                    <h1>Course Search</h1>
+                    <h2>Search Bar</h2>
+                </Col>
+                <Col md={6} style={{ "border-left": "1px solid black" }}>
+                    <CoursePreview course={null} />
+                </Col>
+            </Row>
+        </Container>
         </main>
     );
 }
