258847225936d3aa61af8db3f14cb4022eb9f4bc
HAGENSV22 <hagensv22@gcc.edu>
2025-04-04 14:40:46 -0400

    Course interface

---
+++ frontend/src/interface/course.ts
@@ -0,0 +1,11 @@
+interface Course {
+    title: string;
+    department: string;
+    code: number;
+    section: string;
+    credits: number;
+    description: string;
+    professor: string;
+}
+
+export type { Course }
