2acf03198216f4185bf9d88021e25d5e1ad6bc62
HAGENSV22 <hagensv22@gcc.edu>
2025-04-07 10:10:23 -0400

    Move format time to course

--- frontend/src/interface/course.ts
+++ frontend/src/interface/course.ts
@@ -1,4 +1,5 @@
 interface Course {
+    cid: number;
     name: string;
     department: string;
     courseCode: number;
@@ -12,4 +13,47 @@ interface Course {
     duration: number;
 }
 
+const formatTime = (course: Course) => {
+    const mwf = formatTimeMWF(course);
+    const tr = formatTimeTR(course);
+
+    if (mwf.length !== 1){
+        return tr.length === 1 ? mwf : `${mwf}, ${tr}`
+    } else {
+        return tr
+    }
+}
+
+const toTimeString = (time: number) => {
+    const hour = 8+Math.floor(time/60)
+    const minute = time%60
+
+    return `${(hour-1)%12+1}:${minute < 10 ? "0" : ""}${minute}${hour <= 12 ? "AM" : "PM"}`
+}
+
+const formatTimeMWF = (course: Course) => {
+    const days = ["M","T","W","R","F"];
+    let time = ["",""]
+    for (let i = 0; i < 5; i += 2){
+        if (course.startTime[i] != -1){
+            time[0] += days[i];
+            time[1] = `${toTimeString(course.startTime[i])} - ${toTimeString(course.startTime[i]+course.duration)}`
+        }
+    }
+    return time.join(" ")
+}
+
+const formatTimeTR = (course: Course) => {
+    const days = ["M","T","W","R","F"];
+    let time = ["",""]
+    for (let i = 1; i < 5; i += 2){
+        if (course.startTime[i] != -1){
+            time[0] += days[i];
+            time[1] = `${toTimeString(course.startTime[i])} - ${toTimeString(course.startTime[i]+course.duration)}`
+        }
+    }
+    return time.join(" ")
+}
+
+export { formatTime }
 export type { Course }
