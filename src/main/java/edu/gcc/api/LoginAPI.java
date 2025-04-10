948c1189c0bb68391284ab4a681929c64fd86026
HAGENSV22 <hagensv22@gcc.edu>
2025-04-10 14:04:45 -0400

    Create login api

---
+++ src/main/java/edu/gcc/api/LoginAPI.java
@@ -0,0 +1,22 @@
+package edu.gcc.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+public class LoginAPI {
+//    private final AuthenticationManager authenticationManager;
+//
+//    public LoginController(AuthenticationManager authenticationManager) {
+//        this.authenticationManager = authenticationManager;
+//    }
+//
+//    @PostMapping("/login")
+//    public ResponseEntity login(@RequestBody LoginRequest loginRequest) {
+//        Authentication authenticationRequest = UsernamePasswordAuthenticationToken.unauthenticated(loginRequest.username(), loginRequest.password());
+//        Authentication authenticationResponse = authenticationManager.authenticate(authenticationRequest);
+//        // ...
+//    }
+//
+//    public record LoginRequest(String username, String password) {}
+}
