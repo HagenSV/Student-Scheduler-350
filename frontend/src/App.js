31f537de31beb9d56e60b42bb35f681c80b34085
HAGENSV22 <hagensv22@gcc.edu>
2025-04-11 18:18:15 -0400

    Add login and registration pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import HeaderNav from './components/header_nav/HeaderNav.tsx';
 import Schedule from './pages/Schedule.tsx';
 import Search from './pages/Search.tsx';
 import Profile from './pages/Profile.tsx';
+import Login from './pages/Login.tsx';
+import Register from './pages/Register.tsx';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
             <Route path="/" element={<Schedule />} />
             <Route path="/search" element={<Search />} />
             <Route path="/profile" element={<Profile />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
         </Routes>
       </Router>
     </>
