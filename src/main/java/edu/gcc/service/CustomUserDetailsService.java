f815387a2c90702d441db714d6bdd6ecf9f156c8
HAGENSV22 <hagensv22@gcc.edu>
2025-04-11 18:15:14 -0400

    Move CustomUserDetailsService.java to services package

---
+++ src/main/java/edu/gcc/service/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package edu.gcc.service;
+
+import edu.gcc.DBUser;
+import edu.gcc.UserRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        DBUser dbUser = userRepository.findByEmail(email);
+        if (dbUser == null){
+            throw new UsernameNotFoundException("User not found");
+        }
+        return User.builder()
+                .username(dbUser.getEmail())
+                .password(dbUser.getPassword())
+                .roles("USER")
+                .build();
+        throw new UsernameNotFoundException("Not Implemented");
+    }
+}
