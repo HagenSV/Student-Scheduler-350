615246e37974e1ac3d3a74f9b54de4163c407a09
HAGENSV22 <hagensv22@gcc.edu>
2025-04-12 16:56:10 -0400

    Fix login form to match expected format

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -5,8 +5,8 @@ const Login = () => {
     return (
         <form method='POST'>
             <h2>Student Scheduler</h2>
-            <input name="user" type="text" placeholder="username" /><br />
-            <input name="pass" type="password" placeholder="password" /><br />
+            <input name="username" type="text" placeholder="email" /><br />
+            <input name="password" type="password" placeholder="password" /><br />
             <input className="btn primary" type="submit" value="Log In" /><br />
             <a className="btn secondary" href="/register">Create Account</a><br />
             <a href="/recover">Forgot Password?</a><br />
