ede6e2d908e0688ffdbe71425f87174f2c2a8396
HAGENSV22 <hagensv22@gcc.edu>
2025-04-14 16:56:47 -0400

    Remove methods from UserRepository

--- src/main/java/edu/gcc/repository/UserRepository.java
+++ src/main/java/edu/gcc/repository/UserRepository.java
@@ -6,6 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<dbUser,String> {
     dbUser findByUsername(String name);
     boolean existsByUsername(String name);
-    boolean existsByUid(String uid);
-    void deleteByUid(String uid);
 }
