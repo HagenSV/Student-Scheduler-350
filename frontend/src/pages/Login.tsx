9b08209e89f2b32a010f8f66a603bd4aadc89597
HAGENSV22 <hagensv22@gcc.edu>
2025-04-14 16:08:36 -0400

    Update login and registration to use username

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -5,7 +5,7 @@ const Login = () => {
     return (
         <form method='POST'>
             <h2>Student Scheduler</h2>
-            <input name="username" type="text" placeholder="email" /><br />
+            <input name="username" type="text" placeholder="username" /><br />
             <input name="password" type="password" placeholder="password" /><br />
             <input className="btn primary" type="submit" value="Log In" /><br />
             <a className="btn secondary" href="/register">Create Account</a><br />
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -4,9 +4,9 @@ const Register = () => {
     return (
         <form method='POST'>
             <h2>Student Scheduler</h2>
-            <input name="email" type="text" placeholder="email" /><br />
-            <input name="confirm" type="text" placeholder="confirm email" /><br />
-            <input name="password" type="password" placeholder="password" /><br />
+            <input name="username" type="text" placeholder="username" /><br />
+            <input name="password" type="text" placeholder="password" /><br />
+            <input name="confirmPassword" type="password" placeholder="confirm password" /><br />
             <input className="btn primary" type="submit" value="Register" /><br />
             <a className="btn secondary" href="/register">Login</a><br />
             <a href="/recover">Forgot Password?</a><br />
