1b2211c043bb8b8fbc6996a240c130540c0bcae0
HAGENSV22 <hagensv22@gcc.edu>
2025-04-14 16:57:00 -0400

    Fix registration page

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -5,10 +5,10 @@ const Register = () => {
         <form method='POST'>
             <h2>Student Scheduler</h2>
             <input name="username" type="text" placeholder="username" /><br />
-            <input name="password" type="text" placeholder="password" /><br />
+            <input name="password" type="password" placeholder="password" /><br />
             <input name="confirmPassword" type="password" placeholder="confirm password" /><br />
             <input className="btn primary" type="submit" value="Register" /><br />
-            <a className="btn secondary" href="/register">Login</a><br />
+            <a className="btn secondary" href="/login">Login</a><br />
             <a href="/recover">Forgot Password?</a><br />
         </form>
     )
