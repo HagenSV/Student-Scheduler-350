3c83cabe344b0cead9dc68d5289a3f6492d545c4
HAGENSV22 <hagensv22@gcc.edu>
2025-04-14 13:36:30 -0400

    Fix format

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -5,8 +5,8 @@ const Register = () => {
         <form method='POST'>
             <h2>Student Scheduler</h2>
             <input name="email" type="text" placeholder="email" /><br />
-            <input name="confirm" type="text" placeholder="email" /><br />
-            <input name="pass" type="password" placeholder="password" /><br />
+            <input name="confirm" type="text" placeholder="confirm email" /><br />
+            <input name="password" type="password" placeholder="password" /><br />
             <input className="btn primary" type="submit" value="Register" /><br />
             <a className="btn secondary" href="/register">Login</a><br />
             <a href="/recover">Forgot Password?</a><br />
