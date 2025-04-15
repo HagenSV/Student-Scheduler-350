e14f5ccd061e57ccf3461475e181ca49e8aef113
HAGENSV22 <hagensv22@gcc.edu>
2025-04-15 19:30:29 -0400

    Account page template

---
+++ frontend/src/pages/Account.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Account = () => {
+    return (
+        <main>
+        <h1>My Account</h1>
+        <p style={{ fontWeight: "bold" }}>Major(s):</p>
+        <ul>
+        {/* TODO: List majors */}
+        </ul>
+        <p style={{ fontWeight: "bold" }}>Minor(s):</p>
+        <ul>
+        {/* TODO: List minors */}
+        </ul>
+        <h1>Completed Courses</h1>
+        </main>
+    )
+}
+
+export default Account;
