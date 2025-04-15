043432fe3cd53744ed05eb29dd11eb3430b2e099
HAGENSV22 <hagensv22@gcc.edu>
2025-04-15 19:29:42 -0400

    Add account page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import HeaderNav from './components/header_nav/HeaderNav.tsx';
 import Schedule from './pages/Schedule.tsx';
 import Search from './pages/Search.tsx';
+import Account from './pages/Account.tsx';
 import Profile from './pages/Profile.tsx';
 import Login from './pages/Login.tsx';
 import Register from './pages/Register.tsx';
@@ -15,6 +16,7 @@ function App() {
             <Route path="/" element={<Schedule />} />
             <Route path="/search" element={<Search />} />
             <Route path="/profile" element={<Profile />} />
+            <Route path="/account" element={<Account />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
         </Routes>
