e1107cd17f4f17253c9d0b384048cc31904acdb7
HAGENSV22 <hagensv22@gcc.edu>
2025-04-16 14:58:12 -0400

    Fix import path

--- frontend/src/pages/Schedule.tsx
+++ frontend/src/pages/Schedule.tsx
@@ -3,25 +3,22 @@ import CourseListing from '../components/course_listing/CourseListing';
 import scheduleAPI from '../api/schedule';
 import search from '../api/search';
 import { Course, toTimeString } from '../interface/course';
-import CourseTable from '../components/course_stable/CourseTable';
+import CourseTable from '../components/course_table/CourseTable';
 
 const days = [0,1,2,3,4]
 const times = [0, 15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 180, 195, 210, 225, 240, 255, 270, 285, 300, 315, 330, 345, 360, 375, 390, 405, 420, 435, 450, 465, 480, 495, 510, 525, 540, 555, 570, 585, 600, 615, 630, 645, 660, 675, 690, 705, 720, 735, 750, 765, 780, 795, 810]
 
 const Schedule = () => {
-    const [scheduleQueried, setQueried] = useState(false);
     const [courses,setCourses] = useState<Course[]>([]);
 
-    const getSchedule = async () => {
-        setQueried(true)
+    useEffect(() => {
         try {
             const result = await search("COMP 141")
             //const result = await scheduleAPI.getSchedule()
-            if (result !== courses){
-                setCourses(result);
-            }
+            setCourses(result);
         } catch (e){
             console.log(e)
+            Console.log("Failed to get schedule")
         }
     }
 
