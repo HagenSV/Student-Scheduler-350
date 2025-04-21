af4564d4df682bfed0aa2bd8a673ff5b6c00f92b
WHALEYDS22 <whaleyds22@gcc.edu>
2025-04-21 19:07:50 -0400

    Changed ScheduleAPI schedule the constructor is given null values

--- src/main/java/edu/gcc/api/ScheduleAPI.java
+++ src/main/java/edu/gcc/api/ScheduleAPI.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 public class ScheduleAPI {
 
-    private final Schedule schedule = new Schedule();
+    private final Schedule schedule = new Schedule(null, null);
 
     @GetMapping("/api/v1/schedule")
     public List<Course> getSchedule() {
