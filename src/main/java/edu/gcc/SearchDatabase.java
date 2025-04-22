3f8ac81367d2d28af3a8d10988d2ad591ed59329
WHALEYDS22 <whaleyds22@gcc.edu>
2025-04-21 22:30:57 -0400

    Fixed a bug where it would return null instead of an empty course array if there were no classes in the schedule

--- src/main/java/edu/gcc/SearchDatabase.java
+++ src/main/java/edu/gcc/SearchDatabase.java
@@ -78,6 +78,9 @@ public class SearchDatabase {
         }catch (SQLException e){
             e.printStackTrace();
         }
+        if(toReturn.isEmpty()){
+            return new ArrayList<>();
+        }
         return toReturn;
     }
 
