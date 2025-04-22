b3b0dabfe575f42e811e5b5db480475cc6825b77
HAGENSV22 <hagensv22@gcc.edu>
2025-04-22 09:38:24 -0400

    Update profile

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CourseTable from '../components/course_table/CourseTable';
 
 const Profile = () => {
     return (
@@ -6,10 +7,14 @@ const Profile = () => {
         <h1>My Info</h1>
         <h2>Major(s)</h2>
         <p>Here is where you can view your major.</p>
+        <ul>
+        </ul>
         <h2>Minor(s)</h2>
         <p>Here you can see your minor.</p>
-        <h2>Completed Courses</h2>
-        <p>This is where you can see a list of your completed courses</p>
+        <ul>
+        </ul>
+        <h1>Completed Courses</h1>
+        <CourseTable courses={[]} remove={undefined}/>
         </main>
     );
 }
