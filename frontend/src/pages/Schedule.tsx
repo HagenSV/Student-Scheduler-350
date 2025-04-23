0a3c2efaa20acdd97d39c042a91a84215012ad46
HAGENSV22 <hagensv22@gcc.edu>
2025-04-23 16:26:30 -0400

    Fix api endpoints

--- frontend/src/pages/Schedule.tsx
+++ frontend/src/pages/Schedule.tsx
@@ -71,9 +71,9 @@ const Schedule = () => {
         <CourseTable courses={courses} remove={removeCourse}/>
         {!courses && <p>Nothing to see here, try adding a course!</p>}
         <h1 id="export">Export</h1>
-        <p><a href="/api/v1/schedule/export?format=email">Email Schedule</a></p>
-        <p><a href="/api/v1/schedule/export?format=pdf">Export to PDF</a></p>
-        <p><a href="/api/v1/schedule/export?format=google">Export to Google Calendar</a></p>
+        <p><a href="/api/v1/export/email">Email Schedule</a></p>
+        <p><a href="/api/v1/export/pdf">Export to PDF</a></p>
+        <p><a href="/api/v1/export/googleCalendar">Export to Google Calendar</a></p>
         </main>
     );
 }
